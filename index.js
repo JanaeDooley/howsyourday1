09735a7b3d7fbaa48b3cb773647c8286d76cb047
Janae Dooley <janaesdooley@gmail.com>
2021-12-05 10:37:46 -0600

    finished with the project completely

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function howIsYourDay(){
     const messages1 = ['Your Day is gonna be horrible', 'Be on the Lookout for ladders or broken glass', 'Be wary, but you are gonna be find', 'You woke up on the right side of bed', 'You got lady luck on your side'];
-    const result = Math.floor(math.random()* messages1.length);
-    console.log(messages[result]);
+    const result = Math.floor(Math.random()* messages1.length);
+    console.log(messages1[result]);
 }
+
+howIsYourDay();
